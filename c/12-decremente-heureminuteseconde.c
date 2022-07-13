4386849f9f96980ce9da6cb7f9da0a58ff8c3351
benfredj <ouissem.benfredj@gmail.com>
2022-07-13 14:41:16 +0100

    first commmit

--- c/12-decremente-heureminuteseconde.c
+++ c/12-decremente-heureminuteseconde.c
@@ -10,8 +10,22 @@ int main(){
 	printf("seconde:");
 	scanf("%d",&seconde);
 	if(heure<=23 && minute<=59 && seconde<=59){
-		
-		printf("%d:%d:%d",heure,minute,seconde);
+		if(seconde==0){
+        seconde = 59;
+        if(minute==0){
+           minute = 59;
+           if(heure==0){
+               heure = 23;
+           } else{
+               heure--;
+           }
+        }else{
+            minute--;
+        }
+    }else{
+        seconde--;
+    }    
+		printf("heure decremente: %d:%d:%d",heure,minute,seconde);
 	}else{
 		printf("heure invalide");
 	}
